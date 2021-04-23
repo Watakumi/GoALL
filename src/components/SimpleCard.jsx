867feb6adcbc5fd66f731a9da594f70b2c778de9
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-23 23:24:10 +0900

    fix lint

--- src/components/SimpleCard.jsx
+++ src/components/SimpleCard.jsx
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
   root: {
@@ -35,8 +36,15 @@ export default function SimpleCard({ goal }) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">{goal.actionName}</Button>
+        <Button size="small">編集</Button>
       </CardActions>
     </Card>
   );
 }
+
+SimpleCard.propTypes = {
+  goal: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+  }).isRequired,
+};
