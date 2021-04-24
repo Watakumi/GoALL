d8421f6522ae58fce66a708f6d7dcb760309bf4e
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-24 23:21:40 +0900

    fix lint

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -3,12 +3,10 @@ import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Divider from '@material-ui/core/Divider';
 import Box from '@material-ui/core/Box';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import LabelIcon from '@material-ui/icons/Label';
 import List from '@material-ui/core/List';
 import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import { mainListItems, SecondaryListItems } from '../components/listItems';
 import ListItemLink from '../components/ListItemLink';
 
@@ -46,3 +44,7 @@ const Sidebar = ({ onClick }) => {
 };
 
 export default Sidebar;
+
+Sidebar.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
