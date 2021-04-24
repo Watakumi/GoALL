a64ad4dfab72ff525794ffc57ac461d1fb7beb46
石渡拓海 <t-ishiwata@sight-visit.com>
2021-04-24 23:16:36 +0900

    fix lint

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,12 +7,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import {
-  HashRouter, Route, Switch, Link as RouterLink,
-} from 'react-router-dom';
+import { HashRouter, Route, Switch, Link as RouterLink } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Main from './Main';
 import LabelList from './LabelList';
@@ -117,7 +113,6 @@ export default function Dashboard() {
             <MenuIcon />
           </IconButton>
           <Typography
-            component="h1"
             variant="h6"
             color="inherit"
             noWrap
